664c846eda21600b386d4da3b485722969ad2f3b
Anant Chandra <anantcha@usc.edu>
2024-11-12 16:08:35 +0530

    Add KG retriever

--- markov_ai/__init__.py
+++ markov_ai/__init__.py
@@ -1,6 +1,7 @@
-from .destination import DatabaseService, Destination
 from .component import Component
 from .components import DataLoader, PreProcessor
+from .destination import DatabaseService, Destination
 from .pipeline import Pipeline
+from .retriever import Retriever
 
-__all__ = ["Component", "DataLoader", "DatabaseService", "Destination", "Pipeline", "PreProcessor"]
+__all__ = ["Component", "DataLoader", "DatabaseService", "Destination", "Pipeline", "PreProcessor", "Retriever"]
